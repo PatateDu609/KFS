327d4c5f190bf2fe4de0a4708f0479fe8a04a847
Ghali Boucetta <gboucett@student.42.fr>
2023-11-03 22:03:01 +0100

    Add base kmalloc function

--- src/system/memory/alloc.c
+++ src/system/memory/alloc.c
@@ -1,5 +1,45 @@
 #include "mem/allocator.h"
+#include <stdbool.h>
 
 void *kmalloc(size_t size) {
-	return NULL;
+	static const size_t bits = sizeof(*phys_alloc.bitmap) * 8;
+
+	size = ALIGN(size, FRAME_SIZE);
+	size_t nb_pages = size / FRAME_SIZE;
+	size_t start_search = phys_alloc.last_alloc_bit == 0 ? 0 : phys_alloc.last_alloc_bit + 1;
+
+	size_t streak = 0, page = start_search;
+	for (size_t i = page / bits; i < phys_alloc.bitmap_size && streak < nb_pages; i++) {
+		uint32_t byte = phys_alloc.bitmap[i];
+
+		for (size_t j = 0, mask = 1; j < bits && streak < nb_pages; j++, mask <<= 1) {
+			if (byte & mask) {
+				streak = 0;
+			} else if (!streak) {
+				page = i * bits + j;
+			}
+
+			streak++;
+		}
+	}
+
+	if (streak < nb_pages)
+		return NULL;
+
+	size_t byte = page / bits;
+	size_t offset = page % bits;
+	for (size_t remaining = nb_pages; remaining > 0; remaining--) {
+		uint32_t mask = 0;
+
+		for (size_t i = offset; remaining > 0 && i < bits; i++, remaining--)
+			mask |= 1 << i;
+
+		phys_alloc.bitmap[byte] |= mask;
+
+		if (offset >= bits) {
+			byte++;
+			offset = 0;
+		}
+	}
+	return phys_alloc.data + (page * FRAME_SIZE);
 }
--- src/system/memory/bitmap.c
+++ src/system/memory/bitmap.c
@@ -1,14 +1,16 @@
 #include "mem/allocator.h"
 #include "mem/physical.h"
+#include <string.h>
 
 void init_bitmap(ptrdiff_t kern_size) {
 	uint32_t nb_pages = phys_alloc.entry->len / FRAME_SIZE;
 	uint32_t bitmap_size = ALIGN(nb_pages / 8 + nb_pages % 8, FRAME_SIZE);
 
-	phys_alloc.data = (uint32_t *) ALIGN((uint32_t) phys_alloc.entry->addr + kern_size + bitmap_size, FRAME_SIZE);
+	phys_alloc.data = (uint32_t *) ALIGN((ptrdiff_t)phys_alloc.entry->addr, FRAME_SIZE);
 	phys_alloc.nb_pages = nb_pages;
 	phys_alloc.bitmap = (uint32_t *) ALIGN((uint32_t) phys_alloc.entry->addr + kern_size, FRAME_SIZE);
 	phys_alloc.bitmap_size = bitmap_size;
+	memset(phys_alloc.bitmap, 0, phys_alloc.bitmap_size);
 
 	phys_alloc.last_alloc_bit = 0;
 
